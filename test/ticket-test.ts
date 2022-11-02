d0e7eccdafda68afe9d12181d144f37bdf4f9fca
beauwilliams <beau.williams@icloud.com>
2022-11-02 11:26:11 +1100

    fix: tests

--- test/ticket-test.ts
+++ test/ticket-test.ts
@@ -2,14 +2,14 @@ import { expect } from "chai";
 import { ethers } from "hardhat";
 
 describe("Ticket", () => {
-  let ticket, addr1: any, ticketContract: any, owner: any, toAddress: any;
+  let ticket, ticketReceiver: any, ticketContract: any, owner: any, toAddress: any;
   let headurl: string, tailurl: string;
   before(async () => {
     headurl =
       "https://bafybeiapj4f5pbsygydhq4ds5qevobdj3wjvdoybpkr6gctynwegqokbzq.ipfs.nftstorage.link/";
     tailurl = ".json";
     ticket = await ethers.getContractFactory("Ticket");
-    [owner, addr1, toAddress] = await ethers.getSigners();
+    [owner, ticketReceiver, toAddress] = await ethers.getSigners();
     ticketContract = await ticket.deploy();
   });
 
@@ -21,21 +21,21 @@ describe("Ticket", () => {
 
   describe("Mint token", () => {
     it("Should mint token", async function () {
-      await ticketContract.connect(owner).mintToken(addr1.address, 2);
+      await ticketContract.connect(owner).mintToken(ticketReceiver.address, 42);
 
       expect(await ticketContract.tokenURI(1)).to.equal(
         headurl + "1" + tailurl
       );
-      expect(await ticketContract.ownerOf(1)).to.equal(addr1.address);
-      expect(await ticketContract.tokenURI(2)).to.equal(
-        headurl + "2" + tailurl
+      expect(await ticketContract.ownerOf(1)).to.equal(ticketReceiver.address);
+      expect(await ticketContract.tokenURI(42)).to.equal(
+        headurl + "42" + tailurl
       );
-      expect(await ticketContract.ownerOf(2)).to.equal(addr1.address);
+      expect(await ticketContract.ownerOf(42)).to.equal(ticketReceiver.address);
     });
 
     it("Should fail to exceed supply cap", async function () {
       await expect(
-        ticketContract.connect(owner).mintToken(addr1.address, 200)
+        ticketContract.connect(owner).mintToken(ticketReceiver.address, 200)
       ).to.be.revertedWith("SupplyCapExceeded");
     });
 
